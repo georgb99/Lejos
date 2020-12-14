2e6e58dc95d54eed9840428db90523f4a510765e
kruse <kruse@DESKTOP-RCQ7ABM.fritz.box>
2020-12-14 11:04:18 +0100

    Complete

---
+++ SvenGeorgRobo/src/svenGeorgMain/main.java
@@ -0,0 +1,16 @@
+package svenGeorgMain;
+
+
+import lejos.hardware.Sound;
+import lejos.utility.Delay;
+import plott3r_V1_solved.*;
+import positions.Position3D;
+
+public class main {
+	public static void main(String args[]) throws InterruptedException {
+		Roboter roboter = new Roboter();
+		roboter.meinProgramm(roboter);
+	}
+	
+	
+}
